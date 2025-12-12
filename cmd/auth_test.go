4a2bcdf25389d2809ca78f0b06fb1c184bdcbc50
KarenTsaturyan <karentsaturyan2002@gmail.com>
2025-12-12 23:29:52 +0400

    E2E Testing

---
+++ cmd/auth_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"http_5/internal/auth"
+	"http_5/internal/user"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func initDb() *gorm.DB {
+	// cmd/.env
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func initData(db *gorm.DB) {
+	db.Create(&user.User{
+		Email:    "abc@a.com",
+		Password: "$2a$10$kbSAmj.MD.X5gw57Y7vCkutpSAf4tvm6MSPLRJXUR1BuUt4Mj1dHC",
+		Name:     "Test",
+	})
+}
+
+func removeData(db *gorm.DB) {
+	// Unscoped disables the global scope of soft deletion in a query.
+	db.Unscoped().
+		Where("email = ?", "abc@a.com").
+		Delete(&user.User{})
+}
+
+// E2E tests
+// go run .\migrations\auto.go for link_test DB
+func TestLoginSuccess(t *testing.T) {
+	// Prepare
+	db := initDb()
+	initData(db)
+
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "abc@a.com",
+		Password: "hello",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected %d got %d", 200, res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resData.Token == "" {
+		t.Fatal("Token empty")
+	}
+	removeData(db)
+}
+
+func TestLoginFail(t *testing.T) {
+	db := initDb()
+	initData(db)
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "as2@f.ru",
+		Password: "helloFail",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 401 {
+		t.Fatalf("Expected %d got %d", 401, res.StatusCode)
+	}
+	removeData(db)
+}
